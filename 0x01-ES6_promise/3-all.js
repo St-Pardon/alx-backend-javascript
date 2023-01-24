4cd344cf401554c47940bb302ae8b775d847aef2
St-Pardon <okc4pardon@gmail.com>
2023-01-24 18:36:29 +0000

    :wrench: removed error object for task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,5 +4,5 @@ export default function handleProfileSignup() {
   return Promise
     .all([uploadPhoto(), createUser()]) // imported are functions
     .then((data) => console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`))
-    .catch(() => console.log(new Error('Signup system offline')));
+    .catch(() => console.log('Signup system offline'));
 }
