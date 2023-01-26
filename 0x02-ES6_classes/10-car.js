3307854b6a1f503b2ca6d8dea992e863b869b853
Onyedikachi Onu <53058358+St-Pardon@users.noreply.github.com>
2023-01-26 22:31:38 +0100

    :white_check_mark: completed task 10

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,41 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  set brand(val) {
+    this._brand = val;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set motor(val) {
+    this._motor = val;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(value) {
+    this._color = value;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+
+    return new Species();
+  }
+}
