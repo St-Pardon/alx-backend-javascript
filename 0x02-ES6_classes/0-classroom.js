76e76577bb561713e78358c07ca5dc251b41567b
St-Pardon <okc4pardon@gmail.com>
2023-01-26 18:42:05 +0000

    :white_check_mark: completed task 0

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,13 @@
+/**
+ * Javascript Class
+ */
+export default class ClassRoom {
+  /**
+   * Creates a new @see {@link ClassRoom}.
+   * @param {Number} maxStudentsSize - The maximum number of
+   * students in the class.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
