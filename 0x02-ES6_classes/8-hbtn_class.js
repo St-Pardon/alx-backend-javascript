6858492dc8d79c77760f80a6e9b05b2fce55b39e
Onyedikachi Onu <53058358+St-Pardon@users.noreply.github.com>
2023-01-26 22:26:43 +0100

    :white_check_mark: completed task 8

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,32 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this.size = size;
+    this.location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(val) {
+    this._size = val;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(val) {
+    this._location = val;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this.size;
+    }
+    if (hint === 'string') {
+      return this.location;
+    }
+    return this;
+  }
+}
