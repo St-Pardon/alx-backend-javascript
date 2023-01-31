6eed8bf156ba1847123d6795aafe610d94938687
St Pardon <okc4pardon@gmail.com>
2023-01-31 19:38:42 +0100

    :white_check_mark: completed advanced task 1

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,10 @@
+export const weakMap = new WeakMap();
+
+/**
+ * Track within the weakMap the number of times queryAPI is called for each endpoint.
+ * @param {WeakMap} endpoint - weakmap of http response
+ */
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) weakMap.set(endpoint, 0);
+  if (weakMap.get(endpoint) >= 5) throw new Error('Endpoint load is high');
+}
