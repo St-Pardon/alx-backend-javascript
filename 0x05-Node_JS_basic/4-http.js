709bd5aedf3cde5a488f5858bc78acdd0c7a7da8
St Pardon <okc4pardon@gmail.com>
2023-03-20 19:30:02 +0100

    :white_check_mark: task 4 node server

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const PORT = 1245;
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+app.listen(PORT, '127.0.0.1', () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
