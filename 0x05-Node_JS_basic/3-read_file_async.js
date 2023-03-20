cc0db6d2c06d38d2992112ae69525824f6613526
St Pardon <okc4pardon@gmail.com>
2023-03-20 20:35:27 +0100

    :wrench: test 2 for task 3

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -4,41 +4,43 @@ function countStudents(path) {
   return new Promise((resolve, reject) => (
     readFile(path, 'utf8', (err, data) => {
       if (err) reject(new Error('Cannot load the database'));
-      const list = data.split('\n');
-      const field = {};
-      const head = list[0].split(',');
-      const arr = [];
-      for (const i in list) {
-        if (i !== '0') {
-          const obj = {};
-          const item = list[i].split(',');
-          for (const j in item) {
-            if (j) {
-              if (j === '3' && Object.hasOwn(field, item[j])) {
-                field[item[j]] += 1;
+      if (data) {
+        const list = data.toString('utf-8').trim().split('\n');
+        const field = {};
+        const head = list[0].split(',');
+        const arr = [];
+        for (const i in list) {
+          if (i !== '0') {
+            const obj = {};
+            const item = list[i].split(',');
+            for (const j in item) {
+              if (j) {
+                if (j === '3' && Object.hasOwn(field, item[j])) {
+                  field[item[j]] += 1;
+                }
+                if (j === '3' && !Object.hasOwn(field, item[j])) {
+                  field[item[j]] = 1;
+                }
+                obj[head[j]] = item[j];
               }
-              if (j === '3' && !Object.hasOwn(field, item[j])) {
-                field[item[j]] = 1;
-              }
-              obj[head[j]] = item[j];
             }
+            arr.push(obj);
           }
-          arr.push(obj);
         }
-      }
-      console.log(`Number of students: ${list.length - 1}`);
-      const key = Object.keys(field);
-      for (const i in key) {
-        if (i) {
-          console.log(
-            `Number of students in ${key[i]}: ${field[key[i]]}. List: ${arr
-              .filter((item) => item.field === key[i])
-              .map((item) => item.firstname)
-              .join(', ')}`,
-          );
+        console.log(`Number of students: ${list.length - 1}`);
+        const key = Object.keys(field);
+        for (const i in key) {
+          if (i) {
+            console.log(
+              `Number of students in ${key[i]}: ${field[key[i]]}. List: ${arr
+                .filter((item) => item.field === key[i])
+                .map((item) => item.firstname)
+                .join(', ')}`,
+            );
+          }
         }
+        resolve(true);
       }
-      resolve(true);
     })));
 }
 
